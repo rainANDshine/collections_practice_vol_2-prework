fae666a3d87958beec816fdf4be230ad76d2779a
Shun Yao <ysmiracle@hotmail.com>
2018-03-31 06:43:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,5 +26,8 @@ def remove_non_strings(array)
   array.delete_if {|element| element.class != String}
 end
 
-def count_elements()
+def count_elements(array)
+  new_array = []
+  
+  array.each do |
   
